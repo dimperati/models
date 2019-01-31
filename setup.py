2d27f744c1be977172ccbee9454b0ac318611c08
dimperati <42004360+dimperati@users.noreply.github.com>
2019-01-31 17:53:16 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='TensorflowModels',
+    version='0.1.dev',
+    packages=['.',],
+    license='Apache 2.0',
+    long_description=open('README.md').read(),
+)
