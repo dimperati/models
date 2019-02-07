d870a51aa22910b588970ca4675d6b360f268d55
dimperati <42004360+dimperati@users.noreply.github.com>
2019-02-07 13:16:22 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='TensorflowModels',
     version='0.1.dev',
-    packages=['.',],
+    packages=[''],
     license='Apache 2.0',
     long_description=open('README.md').read(),
 )
